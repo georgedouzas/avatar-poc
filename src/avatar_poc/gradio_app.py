c8a93393517d6fd9a0f3e0a5587045125e06338e
georgedouzas <gdouzas@icloud.com>
2025-07-24 18:29:43 +0300

    Use allowed paths

--- src/avatar_poc/gradio_app.py
+++ src/avatar_poc/gradio_app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from pathlib import Path
 from tempfile import mkdtemp
+from importlib.resources import files
 
 from avatar_poc.tts.pipper.main import TTS_MODELS_MAPPING, generate_audio
 from avatar_poc.vidgen.sadtalker.main import generate_video
@@ -27,6 +28,7 @@ def generate(language, text, audio_path, image_path, video_path):
 
 
 def launch():
+    avatar_dir = files('avatar_poc').joinpath('avatars')
     temp_dir = mkdtemp()
     audio_path = Path(temp_dir) / "audio.wav"
     video_path = Path(temp_dir) / "video.mp4"
@@ -34,9 +36,13 @@ def launch():
         gr.Markdown("# Avatar PoC")
 
         with gr.Row():
-            language = gr.Dropdown(choices=list(TTS_MODELS_MAPPING.keys()), label="Select Language", value="English")
+            language = gr.Dropdown(
+                choices=list(TTS_MODELS_MAPPING.keys()),
+                label="Select Language",
+                value="English",
+            )
             text = gr.Textbox(lines=4, label="Enter Text")
-        
+
         gr.Markdown("### Select an Image")
 
         with gr.Row():
@@ -44,32 +50,40 @@ def launch():
             next_btn = gr.Button("Next ➡️")
 
         with gr.Row():
-            image_display = gr.Image(type="filepath", label="Image Preview", height=300, width=300, value=IMAGE_CHOICES[0])
-        
+            image_display = gr.Image(
+                type="filepath",
+                label="Image Preview",
+                height=300,
+                width=300,
+                value=IMAGE_CHOICES[0],
+            )
+
         image_index = gr.State(0)
 
         prev_btn.click(
             fn=lambda idx: change_image(idx, -1),
             inputs=image_index,
-            outputs=[image_display, image_index]
+            outputs=[image_display, image_index],
         )
 
         next_btn.click(
             fn=lambda idx: change_image(idx, 1),
             inputs=image_index,
-            outputs=[image_display, image_index]
+            outputs=[image_display, image_index],
         )
 
         generate_button = gr.Button("Generate Video")
         video_output = gr.Video(label="Generated Video", height=400)
 
         generate_button.click(
-            fn=lambda lang, txt, idx: generate(lang, txt, audio_path, IMAGE_CHOICES[idx], video_path),
+            fn=lambda lang, txt, idx: generate(
+                lang, txt, audio_path, IMAGE_CHOICES[idx], video_path
+            ),
             inputs=[language, text, image_index],
-            outputs=video_output
+            outputs=video_output,
         )
 
-    demo.launch(share=True)
+    demo.launch(share=True, allowed_paths=[str(avatar_dir)])
 
 
 if __name__ == '__main__':
--- src/avatar_poc/vidgen/sadtalker/main.py
+++ src/avatar_poc/vidgen/sadtalker/main.py
@@ -4,11 +4,18 @@ from pathlib import Path
 
 def generate_video(audio_path, image_path, video_path):
     video_dir_path = Path(video_path).parent
-    subprocess.run([
-        'python3.8', 'inference.py',
-        '--driven_audio', audio_path,
-        '--source_image', image_path,
-        '--result_dir', video_dir_path
-    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(
+        [
+            'python3.8',
+            'inference.py',
+            '--driven_audio',
+            audio_path,
+            '--source_image',
+            image_path,
+            '--result_dir',
+            video_dir_path,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     list(video_dir_path.iterdir())[0].rename(video_path)
-    
